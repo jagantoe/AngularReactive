8a723568f7ad765d4e8102fe7b040f6ffb289bd2
Ante Coric <ante.coric@involved-it.be>
2025-04-07 17:50:54 +0200

    Routes cleanup

--- projects/classic/src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { PokemonViewerComponent } from './pages/pokemon-viewer/pokemon-viewer.component';
-
-export const routes: Routes = [
-    { path: '', redirectTo: '/pokemon', pathMatch: 'full' },
-    { path: 'pokemon', component: PokemonViewerComponent },
-    { path: 'pokemon/:id', component: PokemonViewerComponent }
-];
